eba573f1a17eac1fa5eec1744504ddb152653f23
sinedied <noda@free.fr>
2017-01-10 18:54:47 +0100

    Added web UI and mDNS discovery, fixed memory leak

--- rc.lua
+++ rc.lua
@@ -7,8 +7,9 @@
 -- #######################################
 
 -- Global vars
-ssid = "YOUR_SSID"
-pass = "YOUR_PASSWORD"
+ssid = "SSID"
+pass = "PASSWORD"
+hostname = "node-rc"
 pin_up = 1
 pin_down = 2
 last_status = "UNKNOWN"
@@ -58,14 +59,14 @@ end
 
 -- Response format: { status: 200, json: false, data: "Hello" }
 -- If json == false, content is assumed to be HTML
-function sendHttpResponse(conn, response)
+function sendHttpResponse(sck, response)
   if response == nil then
     response = {}
   end
   if response.status == nil then
    response.status = 200
   end
-  local res = "HTTP/1.1 " .. response.status .. " OK\r\n"
+  local res = {"HTTP/1.1 " .. response.status .. " OK\r\n"}
   if response.data ~= nil then
     local type = "text/html"
     local content = response.data
@@ -73,13 +74,32 @@ function sendHttpResponse(conn, response)
       type = "application/json"
       content = json.encode(response.data)
     end
-    res = res .. "Content-Type:" .. type .. "\r\n\r\n" .. content
+    if response.encoding ~= nil then
+      res[1] = res[1] .. "Content-Encoding:" .. response.encoding .. "\r\n"
+    end
+    res[1] = res[1] .. "Content-Type:" .. type .. "\r\n\r\n"
+    while content ~= nil do
+      local len = content:len()
+      res[#res + 1] = content:sub(1, math.min(len, 1024))
+      if len > 1024 then
+        content = content:sub(1025)
+      else
+        content = nil
+      end
+    end
   else
-    res = res .. "\r\n"
+    res[1] = res[1] .. "\r\n"
   end
-  conn:send(res, function()
-    conn:close()
-  end)
+  local function send(s)
+    if #res > 0
+    then s:send(table.remove(res, 1))
+    else
+      s:close()
+      res = nil
+    end
+  end
+  sck:on("sent", send)
+  send(sck)
 end
 
 -- =======================================
@@ -87,6 +107,12 @@ end
 -- Setup wifi
 wifi.setmode(wifi.STATION)
 wifi.sta.config(ssid, pass)
+
+-- Setup mDNS discovery
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function()
+  mdns.register(hostname)
+  wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
+end)
 
 -- Blink led
 led_on = 0
@@ -106,22 +132,38 @@ tmr.alarm(1, 1000, tmr.ALARM_SINGLE, function()
   tmr.unregister(0)
 end)
 
+-- Load web.html file
+if file.open("web.html.gz") then
+  local chunk = file.read()
+  html = ""
+  while chunk ~= nil do
+    html = html .. chunk
+    chunk = file.read()
+  end
+  file.close()
+end
+
 -- Init server
-srv = net.createServer(net.TCP) 
-srv:listen(80, function(conn) 
-  conn:on("receive", function(conn,payload)
-    local request = parseHttpRequest(payload)
-    print("path: " .. request.path) 
-    if request.path == "/up" then
-      triggerPin(pin_up, 200)
-      sendHttpResponse(conn)
-    elseif request.path == "/down" then
-      triggerPin(pin_down, 200)
-      sendHttpResponse(conn)
-    else
-      local html = "NodeMCU RC Status: " .. last_status
-      sendHttpResponse(conn, { data = "<!doctype html><html><body>" .. html .. "</body></html>" })
-    end
-    print(node.heap())
-  end)
+srv = net.createServer(net.TCP)
+
+function receive(sck, data)
+  local request = parseHttpRequest(data)
+  if request.path == "/up" then
+    triggerPin(pin_up, 200)
+    sendHttpResponse(sck)
+  elseif request.path == "/down" then
+    triggerPin(pin_down, 200)
+    sendHttpResponse(sck)
+  elseif request.path == "/status" then
+    sendHttpResponse(sck, { data = last_status })
+  elseif request.path == "/" then
+    sendHttpResponse(sck, { data = html, encoding = "gzip" })
+  else
+    sendHttpResponse(sck, { status = 404 })
+  end
+  collectgarbage()
+end
+
+srv:listen(80, function(conn)
+  conn:on("receive", receive)
 end)
